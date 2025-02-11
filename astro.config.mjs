eea5a9f23da360b122108623ab996369c9f4b843
bakera81 <bakera81@icloud.com>
2025-02-10 22:20:43 -0500

    Upgrade to Astro v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
-import compress from 'astro-compress';
+import compress from '@playform/compress';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
-  experimental: {
-    viewTransitions: true,
-  },
+  viewTransitions: true,
+  // experimental: {
+  //   viewTransitions: true,
+  // },
   integrations: [tailwind(), compress()],
 });
